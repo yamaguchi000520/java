ee17cbfabca40c561fa66f7dcd2d34f04998830f
kei yamaguchi <kei.yamaguchi00520@gmail.com>
2023-01-23 11:51:16 +0000

    add.part4

---
+++ 4.java
@@ -0,0 +1,20 @@
+public class Variable01{
+  public static void main(String[] args){
+    // 変数を定義
+    int num1;
+    String str1;
+    // 変数に値を代入
+    num1 = 10;
+    // Stringに文字を代入
+    str1 = "Hello World!";
+    System.out.println(num1);
+    System.out.println(str1);
+
+    // 再代入
+    num1 = 20;
+    System.out.println(num1);
+
+    int num2 = 30;
+    System.out.println(num2);
+  }
+}
