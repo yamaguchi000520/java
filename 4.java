532d978494078d347c58e288595908a98886d679
kei yamaguchi <kei.yamaguchi00520@gmail.com>
2023-01-23 12:26:01 +0000

    fix.part4

--- 4.java
+++ 4.java
@@ -16,5 +16,19 @@ public class Variable01{
 
     int num2 = 30;
     System.out.println(num2);
+
+    // 再代入できないfinal修飾子
+    final int MIN_NUM = 77;
+    System.out.println(MIN_NUM);
+
+    // 以下課題
+    String str2;
+    str2 = "山口　圭";
+    System.out.println(str2);
+    str2 = "最近は辛いものにハマってます。";
+    System.out.println(str2);
+    int num4;
+    num4 = 123;
+    System.out.println(num4);
   }
 }
